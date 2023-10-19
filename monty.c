ffd6edf34ec117ecef36e1feb1c3c9cd414808b1
Israelshecktar <iamshecktar1996@gmail.com>
2023-10-19 12:50:42 +0100

    implementation of add and nop

--- monty.c
+++ monty.c
@@ -68,6 +68,11 @@ void execute_opcode(char *opcode, char *value,
 		pop(stack, line_number);
 	else if (strcmp(opcode, "swap") == 0)
 		swap(stack, line_number);
+	else if (strcmp(opcode, "add") == 0)
+		add(stack, line_number);
+	else if (strcmp(opcode, "nop") == 0)
+		nop(stack, line_number);
+
 	else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n",
--- opcodes+.c
+++ opcodes+.c
@@ -24,3 +24,36 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = temp;
 	*stack = temp;
 }
+/**
+ * add - adds the top two elements of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number
+ * Return: nothing
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
+/**
+ * nop - doesn't do anything
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number
+ * Return: nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
